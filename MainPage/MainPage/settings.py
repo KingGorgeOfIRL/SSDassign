ae594ff32f28d28621c6528c592abe777c0efc9e
asher lee <asherlee.bxl@gmail.com>
2023-08-02 11:29:48 +0800

    'final'

--- MainPage/MainPage/settings.py
+++ MainPage/MainPage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*bjs&d6ujx5n4ied@73a41^#u03!+sg3w3md4%*^ejh*^&pnse'
+SECRET_KEY = '*bjs&d6ujx5n4ied@73a41^#u03!+sg3w3md4%*^ejh*^&pnse'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
