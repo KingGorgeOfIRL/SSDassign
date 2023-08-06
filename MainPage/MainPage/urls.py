c920c4ce132aa2d72161d20740ee619af5d61440
asher lee <asherlee.bxl@gmail.com>
2023-08-06 13:41:03 +0800

    added cache

--- MainPage/MainPage/urls.py
+++ MainPage/MainPage/urls.py
@@ -24,9 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('ForumPage.urls')),
     path('captcha/',include('captcha.urls')),
-]
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
